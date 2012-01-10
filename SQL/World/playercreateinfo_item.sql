d52391afb59ac830e276c09c5c7bf5ef6a8379f2
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-10 01:20:58 +0100

    Update SQL/World/playercreateinfo_item.sql

--- SQL/World/playercreateinfo_item.sql
+++ SQL/World/playercreateinfo_item.sql
@@ -19,12 +19,82 @@ INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '46978', '-1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '0', '51809', '4');
 
 
--- ST Gerrier gladiateur implacable en plaques Niveau 251
+-- S7 Gerrier gladiateur implacable en plaques
 INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40790', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40810', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40829', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40850', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '1', '40870', '1');
+
+-- S7 Paladin Justification de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40792', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40812', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40831', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40852', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40872', '1');
+
+-- S7 Paladin Rédemption de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40910', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40928', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40934', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40940', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '2', '40964', '1');
+
+-- S7 Chasseur Parure de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41088', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41144', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41158', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41206', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '3', '41218', '1');
+
+-- S7 Voleur Habits de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41651', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41656', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41673', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41684', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '4', '41768', '1');
+
+-- S7 Prêtre Grande tenue de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41916', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41922', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41928', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41935', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41941', '1');
+
+-- S7 Prêtre Investiture de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41855', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41860', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41865', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41870', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '5', '41875', '1');
+
+-- S7 dk Violation de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '40791', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '40811', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '40830', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '40851', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '6', '40871', '1');
+
+-- S7 chaman Tonnepoing de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '40995', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41008', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41020', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41034', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41045', '1');
+
+-- S7 chaman Trembleterre de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41082', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41138', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41152', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41200', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41212', '1');
+
+-- S7 chaman Vague guerrière de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '40994', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41002', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41014', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41028', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '7', '41039', '1');
 
 
 
