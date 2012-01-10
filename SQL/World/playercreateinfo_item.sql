c39969e28cea9785bd0cca9be1c7f867238a554a
Carthagods-Servers <Staff@carthagods-servers.com>
2012-01-10 13:26:55 +0100

    Ajout de set s7 Deruid

--- SQL/World/playercreateinfo_item.sql
+++ SQL/World/playercreateinfo_item.sql
@@ -110,3 +110,23 @@ INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42006', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42012', '1');
 INSERT INTO `playercreateinfo_item` VALUES ('0', '9', '42018', '1');
 
+-- S7 Deruid Peau de fauve de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41282', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41294', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41305', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41317', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41328', '1');
+
+-- S7 Deruid Refuge de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41276', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41288', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41299', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41311', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41322', '1');
+
+-- S7 Deruid Sanctuaire de gladiateur implacable
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41662', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41668', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41679', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41716', '1');
+INSERT INTO `playercreateinfo_item` VALUES ('0', '11', '41774', '1');
